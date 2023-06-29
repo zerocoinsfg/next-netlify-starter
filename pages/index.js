4bd248d33965656d494f766c8ddf356ae879f52e
zerocoinsfg <138159597+zerocoinsfg@users.noreply.github.com>
2023-06-29 12:26:26 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Zero Coins</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Zero Coins" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Contact
         </p>
       </main>
 
